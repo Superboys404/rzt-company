cd79c10184e852b20b2e08ab58d31545c97f917c
Superboys <113122314+Superboys404@users.noreply.github.com>
2026-04-06 11:14:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const menuData = [
-  { name: "Seblak", price: 15000, img: "https://i.imgur.com/vryDGdv.jpeg" },
-  { name: "Chicken Krispy", price: 12000, img: "https://i.imgur.com/g8OtxnI.jpeg" },
-  { name: "Es Teh Manis", price: 5000, img: "https://i.imgur.com/yw53q2B.jpeg" },
-  { name: "Es Buah", price: 8000, img: "https://i.imgur.com/XayabNE.jpeg" },
-  { name: "Kerupuk Ikan", price: 4000, img: "https://i.imgur.com/Jw9PBk9.jpeg" }
+  { name: "Piscok", price: 15000, img: "https://i.imgur.com/Nh2x0fZ.jpeg" },
+  { name: "Matcha Latte", price: 12000, img: "https://i.imgur.com/3FKM86V.jpeg" }
 ];
 
 const menuContainer = document.getElementById("menu");
