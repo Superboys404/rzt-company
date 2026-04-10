3d13babffcec9f412e66f2c64fe7b25fbe52748b
Superboys <113122314+Superboys404@users.noreply.github.com>
2026-04-10 09:42:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuData = [
   { name: "Piscok", price: 10000, img: "https://i.imgur.com/Nh2x0fZ.jpeg" },
   { name: "Matcha Latte", price: 7000, img: "https://i.imgur.com/3FKM86V.jpeg" },
-  { name: "hot chocolate🤤", price: 8000, img: "https://i.imgur.com/7ugSnPj.jpeg" }
+  { name: "hot chocolate🤤", price: 8000, img: "https://i.imgur.com/VVin49p.png" }
 ];
 
 const menuContainer = document.getElementById("menu");
